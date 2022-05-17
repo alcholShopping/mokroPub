b940c81e3ebc89930085054baf6d1642d557e765
GDJ46 <GDJ46@DESKTOP-QA0P0I8>
2022-05-17 16:20:26 +0900

    [원성현,조영인]myReviewList.jsp, MyReviewListController, Dao메서드 생성 및 진행중(마이페이지 내가 쓴 리뷰)

--- src/main/java/dao/ReviewDao.java
+++ src/main/java/dao/ReviewDao.java
@@ -5,7 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import util.DBUtil;
 import vo.*;
@@ -40,7 +42,58 @@ public class ReviewDao {
 					e.printStackTrace();
 				}
 			}		
+	}
+	
+	
+	public List<Map<String, Object>> SelectMyReviewById(int consumerNo){
+		List<Map<String, Object>> reviewList = new ArrayList<Map<String, Object>>();
+		Map<String, Object> re = null;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
 		
+		//사진은 잠깐 NULL설정
+		String sql = " SELECT r.review_no reviewNo,r.picture picture,p.name name, r.content content, r.star star"
+				+ " FROM review r "
+				+ "	INNER JOIN `ORDER` o "
+				+ "	ON r.order_no = o.order_no "
+				+ "	INNER JOIN product p "
+				+ "	ON o.product_no = p.product_no "
+				+ " WHERE o.consumer_no = ? ";
+		
+		try {
+			conn = DBUtil.getConnection();
+			stmt = conn.prepareStatement(sql);
+			stmt.setInt(1, consumerNo);
+			
+			rs = stmt.executeQuery();
+			while(rs.next()) {
+				re = new HashMap<String, Object>();
+				re.put("reviewNo", rs.getInt("reviewNo"));
+				re.put("picture", rs.getString("picture"));
+				re.put("name", rs.getString("name"));
+				re.put("content", rs.getString("content"));
+				re.put("star", rs.getInt("star"));
+	
+				System.out.println(re.get("reviewNo") + " <-- reviewNo SelectMyReviewById() ReviewDao ");
+				System.out.println(re.get("picture") + " <-- picture SelectMyReviewById() ReviewDao ");
+				System.out.println(re.get("name") + " <-- name SelectMyReviewById() ReviewDao ");
+				System.out.println(re.get("content") + " <-- content SelectMyReviewById() ReviewDao ");
+				System.out.println(re.get("star") + " <-- star SelectMyReviewById() ReviewDao ");	
+				
+				reviewList.add(re);
+			}
+			
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}		
+		return reviewList;
 
 	}
 }
