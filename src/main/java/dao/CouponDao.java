8190b6268cbb5f87b1acd33d59d53439b7691249
goodee <goodee@DESKTOP-I9GB2O9>
2022-05-23 15:14:11 +0900

    [조영인,강민창] 하루에 쿠폰 하나씩 수령 로직 구현

--- src/main/java/dao/CouponDao.java
+++ src/main/java/dao/CouponDao.java
@@ -99,6 +99,126 @@ public class CouponDao {
 		} 
 		return UseCouponCount;
 	}
+	
+	// 사용하는 쿠폰의 갯수를 조회
+		public int isDayCouponByDay(int couponNo, int consumerId) {
+			int result = -999;
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			String sql= "  SELECT DATEDIFF(NOW() , update_date) dateDiff "
+					+ " FROM consumer_coupon_list "
+					+ " WHERE coupon_no = ? && consumer_no = ? ";
+			try {
+				conn = DBUtil.getConnection();
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, couponNo);
+				stmt.setInt(2, consumerId);
+				rs=stmt.executeQuery();
+				if(rs.next()) {
+					result = rs.getInt("dateDiff");
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} 
+			return result;
+		}
+	
+	// 받으려는 쿠폰이 존재하는지 판별
+		public int isSameCouponByCouponNum(int couponNo, int consumerId) {
+			int UseCouponCount = 0;
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			ResultSet rs = null;
+			String sql= " select count "
+					+ " FROM consumer_coupon_list "
+					+ " WHERE coupon_no = ? && consumer_no = ? ";
+			try {
+				conn = DBUtil.getConnection();
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, couponNo);
+				stmt.setInt(2, consumerId);
+
+				rs=stmt.executeQuery();
+				if(rs.next()) {
+					UseCouponCount = rs.getInt("count");
+					System.out.println(UseCouponCount);
+				}
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} 
+			return UseCouponCount;
+		}
+	
+	// 데일리 쿠폰 증정(하루 1매씩 총 4개까지 가능)
+		public void insertCouponListByName(int consumerNum, int couponNum) {
+			int UseCouponCount = 0;
+			Connection conn = null;
+			PreparedStatement stmt = null;
+			
+			String sql= " INSERT INTO consumer_coupon_list(coupon_no, consumer_no, `count`, create_date, update_date) "
+					+ " VALUES(?, ?, 1, NOW(), NOW()) ";
+			try {
+				conn = DBUtil.getConnection();
+				stmt = conn.prepareStatement(sql);
+				stmt.setInt(1, couponNum);
+				stmt.setInt(2, consumerNum);
+
+				stmt.executeUpdate();
+
+			} catch (SQLException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			} 
+		}
+		
+		// 데일리 쿠폰 증정(이미 존재한다면 갯수를 하나 추가)
+				public void UpdateCouponListByName(int consumerNum, int couponNum) {
+					int UseCouponCount = 0;
+					Connection conn = null;
+					PreparedStatement stmt = null;
+					
+					String sql= " UPDATE consumer_coupon_list "
+							+ " SET COUNT = COUNT + 1 "
+							+ " WHERE coupon_no = ? && consumer_no = ? ";
+					try {
+						conn = DBUtil.getConnection();
+						stmt = conn.prepareStatement(sql);
+						stmt.setInt(1, couponNum);
+						stmt.setInt(2, consumerNum);
+
+						stmt.executeUpdate();
+
+					} catch (SQLException e) {
+						e.printStackTrace();
+					} finally {
+						try {
+							conn.close();
+						} catch (SQLException e) {
+							e.printStackTrace();
+						}
+					} 
+				}
+
 
 	//쿠폰 갯수가 1개 일시 날짜와 시간을 변경하는 쿼리
 	public void updateCouponValidity(int couponNo) {
